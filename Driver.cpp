5222e6b8e5eab1c32176447432b91a2f588ff345
NathanDDarmawan <92316923+NathanDDarmawan@users.noreply.github.com>
2022-06-15 18:43:20 +0700

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -7,10 +7,34 @@ int main()
     int userInput = 0;
     cout<<"==========================EMPLOYEE DATABASE==========================";
     cout<<"\n1. View Database\n2. Insert new employee\n3. Edit employee data\n4. Delete employee data\n5. Close";
-    cout<<"\n\nInput an action:";
     std::cin >> userInput;
-
     
+    
+    while (userInput != 5){
+    cout<<"\n\nInput an action: ";
+    
+    switch (userInput){
+        case 1:
+            cout << "";
+            break;
+            
+        case 2:
+            cout << "";
+            break;
+            
+        case 3:
+            cout << "";
+            break;
+            
+        case 4:
+            
+            break;
+            
+        case 5:
+        //breaks switch case; closes program
+            break;
+        }
+    }
 
 
 
