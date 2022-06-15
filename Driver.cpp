1193f5d6e30b67b74b8c7a04b22effd2b7dfe4d8
NathanDDarmawan <92316923+NathanDDarmawan@users.noreply.github.com>
2022-06-15 17:54:32 +0700

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,1 +1,31 @@
 #include "./Database.hpp"
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int userInput = 0;
+    cout<<"==========================EMPLOYEE DATABASE==========================";
+    cout<<"\n1. View Database\n2. Insert new employee\n3. Edit employee data\n4. Delete employee data\n5. Close";
+    cout<<"\n\nInput an action:";
+    std::cin >> userInput;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
